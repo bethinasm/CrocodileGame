8edd576da10fb9d0ddeb89dc418e630181772a28
Bethina S.M <bethina.s.moen@gmail.com>
2024-09-06 10:19:00 +0200

    MVC update

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let score = 0;
 
 updateView()
 function updateView() {
-    document.getElementById('app').innerHTML = /*HTML*/ `
+    let html = `
 
         <div class="header"> 
             <div>THE CROCODILE GAME</div> 
@@ -17,7 +17,7 @@ function updateView() {
         </div>
 
         <div class="box">
-            <div class="text"> <b>Which number is bigger? <br> Use > or < </b> </div>
+            <div class="text"> <b>Which number is bigger? <br> Use >  < or = </b> </div>
             <div class="numAndInput"> 
                 <div id="firstNumber"> ${number1} </div>
                 <input id="input">
@@ -38,7 +38,12 @@ function updateView() {
             <div>What do you get if you cross a crocodile and a computer?</div>
             <div>A lot of bytes!</div>
         </div>
-    `;
+    `
+    document.getElementById('app').innerHTML = html;
+}
+
+function getOperatorInput() {
+    return document.getElementById('input').value;    // Er dette lov ?
 }
 
 // CONTROLLER
@@ -47,19 +52,19 @@ function getNewNumber(){
     number1 = Math.floor(Math.random()*10 + 1); // genererer to random tall
     number2 = Math.floor(Math.random()*10 + 1);
         
-    document.getElementById('firstNumber').innerHTML = number1; // oppdaterer tallene i div
-    document.getElementById('secondNumber').innerHTML = number2;
+    // document.getElementById('firstNumber').innerHTML = number1; // oppdaterer tallene i div - skal ikke brukes med MVC
+    // document.getElementById('secondNumber').innerHTML = number2; - skal ikke brukes med MVC
 
     console.log(number1)
     console.log(number2)
         
-    document.getElementById('input').value = ''; // tømmer input feltet
+    // document.getElementById('input').value = ''; // tømmer input feltet - skal ikke brukes med MVC
     
     updateView();
 }
     
 function checkAnswer(){
-    let operatorInput = document.getElementById('input').value;
+    let operatorInput = getOperatorInput();//document.getElementById('input').value; (skal ikke brukes i MVC) // value brukes fordi det ikke er slutt tag på input
     let correctOperator = '';
 
     if (number1 > number2){ // sjekker om operator i input div er riktig
@@ -78,7 +83,7 @@ function checkAnswer(){
         alert('NO, WRONG! I am so disappointed. I will take a point back now.')
     }
 
-    document.getElementById('score').innerHTML = score; // oppdaterer score div
+    //document.getElementById('score').innerHTML = score; // oppdaterer score div - skal ikke brukes med MVC, bare document.getElementById en gang
     
     updateView();
 }
